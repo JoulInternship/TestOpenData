deefe6e5880e835fe445b4ba9afefa272a56c767
Guillaume Wuip <guillaume@wuips.com>
2014-06-06 12:49:06 +0200

    New modal directive

---
+++ war/js/directives/modal.js
@@ -0,0 +1,24 @@
+(function () {
+
+    'use strict';
+
+    window.app.directive('ngModal', function () {
+        return {
+            restrict: 'E',
+            replace: true,
+            scope: {
+                title: "=",
+                message: "=",
+                ok: "=",
+                okFunc: "=",
+                cancel: "=",
+                cancelFunc: "="
+            },
+            templateUrl: 'partials/modal.html',
+            link : function () {
+                console.log('start');
+            }
+        };
+    });
+
+}());
